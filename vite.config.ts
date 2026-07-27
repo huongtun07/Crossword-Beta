0843297c40bd12e282a09ceab43bb1d1338beb75
huongtun07 <huongtun07@gmail.com>
2026-07-28 00:54:58 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    //base: '/Crossword-Beta/'
+    //base: '/Crossword-Beta/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
